b3cffff737fa6cf21887d87ab6cc0edef65caa29
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2018-06-23 01:30:51 +0200

    Don't treat ErrNoRows specially, wrap it like the others, and provide a method to check if the error is NoRows.

--- boil/errors.go
+++ boil/errors.go
@@ -1,23 +1,11 @@
 package boil
 
-type boilErr struct {
-	error
-}
+import (
+	"database/sql"
 
-// WrapErr wraps err in a boilErr
-func WrapErr(err error) error {
-	return boilErr{
-		error: err,
-	}
-}
+	"github.com/pkg/errors"
+)
 
-// Error returns the underlying error string
-func (e boilErr) Error() string {
-	return e.error.Error()
-}
-
-// IsBoilErr checks if err is a boilErr
-func IsBoilErr(err error) bool {
-	_, ok := err.(boilErr)
-	return ok
+func IsErrNoRows(err error) bool {
+	return errors.Cause(err) == sql.ErrNoRows
 }
