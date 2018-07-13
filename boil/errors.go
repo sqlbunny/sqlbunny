7f8b09600c84f37cd2bafa3a924896d3a1c5dd48
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2018-07-13 21:43:56 +0200

    Add IsErrUniqueViolation

--- boil/errors.go
+++ boil/errors.go
@@ -3,9 +3,18 @@ package boil
 import (
 	"database/sql"
 
+	"github.com/lib/pq"
 	"github.com/pkg/errors"
 )
 
 func IsErrNoRows(err error) bool {
 	return errors.Cause(err) == sql.ErrNoRows
+}
+
+func IsErrUniqueViolation(err error) bool {
+	cause := errors.Cause(err)
+	if pqErr, ok := cause.(*pq.Error); ok {
+		return pqErr.Code == "23505"
+	}
+	return false
 }
