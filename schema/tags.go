5af5a4b83c21b6cafbe79de108497d62a6365035
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2019-12-31 00:44:30 +0100

    Remove unused errors in tags.go

--- schema/tags.go
+++ schema/tags.go
@@ -2,17 +2,6 @@ package schema
 
 import (
 	"bytes"
-	"errors"
-)
-
-var (
-	errTagSyntax      = errors.New("bad syntax for struct tag pair")
-	errTagKeySyntax   = errors.New("bad syntax for struct tag key")
-	errTagValueSyntax = errors.New("bad syntax for struct tag value")
-
-	errKeyNotSet      = errors.New("tag key does not exist")
-	errTagNotExist    = errors.New("tag does not exist")
-	errTagKeyMismatch = errors.New("mismatch between key and tag.key")
 )
 
 // Tags represent a set of tags from a single struct field
