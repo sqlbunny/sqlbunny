81ef571ba853f4a44bde83d1be8492771736cde4
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-01-06 01:41:42 +0100

    Rename RenameTable fields

--- operations/rename_table.go
+++ operations/rename_table.go
@@ -8,37 +8,37 @@ import (
 )
 
 type RenameTable struct {
-	OldName string
-	NewName string
+	TableName    string
+	NewTableName string
 }
 
 func (o RenameTable) GetSQL() string {
-	return fmt.Sprintf("ALTER TABLE \"%s\" RENAME TO \"%s\"", o.OldName, o.NewName)
+	return fmt.Sprintf("ALTER TABLE \"%s\" RENAME TO \"%s\"", o.TableName, o.NewTableName)
 }
 
 func (o RenameTable) Dump(w io.Writer) {
 	fmt.Fprint(w, "operations.RenameTable {\n")
-	fmt.Fprint(w, "OldName: "+esc(o.OldName)+",\n")
-	fmt.Fprint(w, "NewName: "+esc(o.NewName)+",\n")
+	fmt.Fprint(w, "TableName: "+esc(o.TableName)+",\n")
+	fmt.Fprint(w, "NewTableName: "+esc(o.NewTableName)+",\n")
 	fmt.Fprint(w, "}")
 }
 
 func (o RenameTable) Apply(s *schema.Schema) error {
-	t, ok := s.Tables[o.OldName]
+	t, ok := s.Tables[o.TableName]
 	if !ok {
-		return fmt.Errorf("no such table: %s", o.OldName)
+		return fmt.Errorf("no such table: %s", o.TableName)
 	}
-	if _, ok := s.Tables[o.NewName]; ok {
-		return fmt.Errorf("destination table already exists: %s", o.NewName)
+	if _, ok := s.Tables[o.NewTableName]; ok {
+		return fmt.Errorf("destination table already exists: %s", o.NewTableName)
 	}
 
-	delete(s.Tables, o.OldName)
-	s.Tables[o.NewName] = t
+	delete(s.Tables, o.TableName)
+	s.Tables[o.NewTableName] = t
 
 	for _, t2 := range s.Tables {
 		for _, fk := range t2.ForeignKeys {
-			if fk.ForeignTable == o.OldName {
-				fk.ForeignTable = o.NewName
+			if fk.ForeignTable == o.TableName {
+				fk.ForeignTable = o.NewTableName
 			}
 		}
 	}
